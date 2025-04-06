0f3fe29da00df2dd284bd937e09ebaa7e559596b
yoyo383 <yoad.winter@gmail.com>
2025-04-06 22:49:04 +0300

    using ffmpeg now

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,39 +1,91 @@
 use std::{
-    io::Write,
+    io::{Read, Write},
     net::{TcpListener, TcpStream},
+    process::{Child, ChildStdin, Command, Stdio},
+    thread,
     time::Instant,
 };
 
-fn handle_connection(mut socket: TcpStream) {
-    let mut monitor_option = None;
-    for mon in xcap::Monitor::all().unwrap() {
-        if mon.is_primary().unwrap() {
-            monitor_option = Some(mon);
-            break;
-        }
-    }
+fn start_ffmpeg() -> Child {
+    let ffmpeg = Command::new("ffmpeg")
+        .args([
+            "-f",
+            "rawvideo",
+            "-pix_fmt",
+            "rgba",
+            "-s",
+            "1920x1080",
+            "-r",
+            "30",
+            "-i",
+            "-",
+            "-c:v",
+            "libx264",
+            "-preset",
+            "ultrafast",
+            "-tune",
+            "zerolatency",
+            "-f",
+            "h264", // Output raw H.264 stream
+            "-",
+        ])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::null())
+        .spawn()
+        .expect("Failed to start ffmpeg");
 
-    let monitor = monitor_option.unwrap();
+    ffmpeg
+}
+
+fn start_screenshot_thread(mut stdin: ChildStdin) {
+    thread::spawn(move || {
+        let mut monitor_option = None;
+        for mon in xcap::Monitor::all().unwrap() {
+            if mon.is_primary().unwrap() {
+                monitor_option = Some(mon);
+                break;
+            }
+        }
+
+        let monitor = monitor_option.unwrap();
+        loop {
+            let original_image = monitor.capture_image().unwrap();
+            let raw_pixels = original_image.as_raw();
+
+            stdin.write_all(&raw_pixels).unwrap();
+        }
+    });
+}
+
+fn handle_connection(mut socket: TcpStream) {
+    let mut command = start_ffmpeg();
+    let stdin = command.stdin.take().unwrap();
+    let mut stdout = command.stdout.take().unwrap();
+
+    start_screenshot_thread(stdin);
 
     let mut now = Instant::now();
+
+    let mut buffer = [0u8; 4096];
 
     loop {
         let new_now = Instant::now();
         let dt = new_now.duration_since(now).as_secs_f32();
         now = new_now;
 
-        let original_image = monitor.capture_image().unwrap();
-        let raw_pixels = original_image.as_raw();
-
-        socket
-            .write(&(raw_pixels.len() as u64).to_be_bytes())
-            .unwrap();
-
-        socket
-            .write_all(&raw_pixels)
-            .expect("Could not send to client");
-
-        // println!("{}", 1. / dt);
+        match stdout.read(&mut buffer) {
+            Ok(0) => break,
+            Ok(n) => {
+                socket.write_all(&(n as u64).to_be_bytes()).unwrap();
+                socket.write_all(&buffer[..n]).unwrap();
+                // println!("{} image size: {}", 1. / dt, n);
+            }
+            Err(e) => {
+                eprintln!("ffmpeg read error: {}", e);
+                break;
+            }
+        }
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,75 @@
 use eframe::egui::load::SizedTexture;
-use eframe::egui::{Context, TextureHandle, Ui};
+use eframe::egui::{Context, Ui};
 use eframe::{egui, NativeOptions};
-use std::io::Read;
+use std::io::{Read, Write};
 use std::net::TcpStream;
+use std::process::{Child, ChildStdin, ChildStdout, Command, Stdio};
+use std::sync::{Arc, Mutex};
+use std::thread;
 use std::time::Instant;
 
-fn main() {
-    let options = NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([600.0, 400.0]),
-        hardware_acceleration: eframe::HardwareAcceleration::Preferred,
-        ..Default::default()
-    };
+fn start_ffmpeg() -> Child {
+    let ffmpeg = Command::new("ffmpeg")
+        .args([
+            "-f", "h264", "-i", "-", "-f", "rawvideo", "-pix_fmt", "rgba", "-",
+        ])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::null())
+        .spawn()
+        .expect("Failed to spawn ffmpeg");
 
-    let _ = eframe::run_native(
-        "Screen Capture",
-        options,
-        Box::new(move |cc| Ok(Box::new(MyApp::new(cc)))),
-    );
+    ffmpeg
+}
+
+fn thread_read_decoded(latest_frame: Arc<Mutex<Option<Vec<u8>>>>, mut stdout: ChildStdout) {
+    thread::spawn(move || {
+        let mut rgba_buffer = vec![0u8; 1920 * 1080 * 4];
+        loop {
+            stdout.read_exact(&mut rgba_buffer).unwrap();
+
+            *latest_frame.lock().unwrap() = Some(rgba_buffer.clone());
+        }
+    });
 }
 
 struct MyApp {
     now: Instant,
     dt: f32,
     socket: TcpStream,
+    stdin: ChildStdin,
+    latest_frame: Arc<Mutex<Option<Vec<u8>>>>,
 }
 
 impl MyApp {
     fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         let socket = TcpStream::connect("127.0.0.1:7643").expect("Couldn't connect to the server.");
 
+        let mut ffmpeg = start_ffmpeg();
+        let stdin = ffmpeg.stdin.take().unwrap();
+        let stdout = ffmpeg.stdout.take().unwrap();
+
+        let latest_frame = Arc::new(Mutex::new(None));
+        let latest_frame_clone = latest_frame.clone();
+
+        thread_read_decoded(latest_frame_clone, stdout);
+
         Self {
             now: Instant::now(),
             dt: 0.,
             socket,
+            stdin,
+            latest_frame,
         }
     }
 
-    fn render(&mut self, ui: &mut Ui, ctx: &Context, handle: TextureHandle) {
-        ui.style_mut().spacing.indent = 0.;
-        let image = SizedTexture::new(&handle, ui.available_size());
-        ui.image(image);
+    fn render(&mut self, ui: &mut Ui, ctx: &Context) {
+        if let Some(image) = &*self.latest_frame.lock().unwrap() {
+            let texture = egui::ColorImage::from_rgba_unmultiplied([1920, 1080], image);
+            let handle = ctx.load_texture("screen", texture, egui::TextureOptions::default());
+            let sized_texture = SizedTexture::new(&handle, ui.available_size());
+            ui.image(sized_texture);
+        }
 
         ui.input(|input| {
             if input.pointer.button_pressed(egui::PointerButton::Primary) {
@@ -57,20 +87,41 @@ impl eframe::App for MyApp {
         self.dt = now.duration_since(self.now).as_secs_f32();
         self.now = now;
 
-        let mut size_buffer = [0u8; 8];
-        self.socket.read_exact(&mut size_buffer).unwrap();
-        let size = u64::from_be_bytes(size_buffer);
+        // println!("{}", 1. / self.dt);
 
-        let mut pixels = vec![0u8; size as usize];
-        self.socket.read_exact(&mut pixels).unwrap();
+        let mut len_buffer = [0u8; 8];
+        self.socket.read_exact(&mut len_buffer).unwrap();
+        let len = u64::from_be_bytes(len_buffer);
 
-        let texture = egui::ColorImage::from_rgba_unmultiplied([1920, 1080], &pixels);
-        let handle = ctx.load_texture("screen", texture, egui::TextureOptions::default());
+        let mut packet = vec![0u8; len as usize];
+        self.socket.read_exact(&mut packet).unwrap();
+
+        self.stdin.write_all(&packet).unwrap();
+
+        // let pixels = png_to_rgba(png_data);
+
+        // let texture = egui::ColorImage::from_rgba_unmultiplied([1920, 1080], &pixels);
+        // let handle = ctx.load_texture("screen", texture, egui::TextureOptions::default());
 
         egui::CentralPanel::default()
             .frame(egui::Frame::NONE)
             .show(ctx, |ui| {
-                self.render(ui, ctx, handle);
+                self.render(ui, ctx);
             });
     }
+}
+
+fn main() {
+    let options = NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([600.0 * 1920.0 / 1080.0, 600.0]),
+        hardware_acceleration: eframe::HardwareAcceleration::Preferred,
+        ..Default::default()
+    };
+
+    let _ = eframe::run_native(
+        "Screen Capture",
+        options,
+        Box::new(move |cc| Ok(Box::new(MyApp::new(cc)))),
+    );
 }
